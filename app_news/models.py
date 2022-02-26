ee8c051eaafd701fadb5df461bb68cd4a4668eee
Lemmy Mwaura <lemminton@gmail.com>
2022-02-26 20:25:17 +0300

    articles model created

---
+++ app_news/models.py
@@ -0,0 +1,10 @@
+class Articles():
+    def __init__(self, author, title, description, url, urltoImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urltoImage = urltoImage
+        self.publishedAt = publishedAt
+        self.content = content
+        
