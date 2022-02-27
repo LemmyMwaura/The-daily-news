c5fe84c72e1b42dbf7034a209a524be3c6ae9bf3
Lemmy Mwaura <lemminton@gmail.com>
2022-02-27 11:34:14 +0300

    Added models for different requests

--- app_news/models.py
+++ app_news/models.py
@@ -7,4 +7,24 @@ class Articles():
         self.urltoImage = urltoImage
         self.publishedAt = publishedAt
         self.content = content
-        
+
+class Categories():
+    def __init__(self, author, title, description, url, urltoImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urltoImage = urltoImage
+        self.publishedAt = publishedAt
+        self.content = content
+       
+class News_Source():
+    def __init__(self, author, title, description, url, urltoImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urltoImage = urltoImage
+        self.publishedAt = publishedAt
+        self.content = content
+       
