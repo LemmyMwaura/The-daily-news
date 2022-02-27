e9d88d6ad230a02ff0eac9864012a66f02a3dc44
Lemmy Mwaura <lemminton@gmail.com>
2022-02-27 14:05:55 +0300

    Views refactored to accept more input

--- app_news/views.py
+++ app_news/views.py
@@ -1,13 +1,14 @@
 from flask import render_template
 from app_news import app
-from .request import process_articles, process_categories, process_news_sources
+from .request import process_all_sources, process_articles, process_categories, process_news_sources
 from datetime import datetime
 
 @app.route('/')
 @app.route('/home')
 def index():
     all_articles = process_articles()
-    return render_template('home.html', articles = all_articles, datetime1=datetime)
+    all_sources = process_all_sources()
+    return render_template('home.html', articles = all_articles, sources = all_sources, datetime1=datetime)
 
 @app.template_filter('datetimeformat')
 def datetimeformat(value, format='%B %d, %Y'):
@@ -16,9 +17,11 @@ def datetimeformat(value, format='%B %d, %Y'):
 @app.route('/categories/<category>')
 def categories(category):
     all_categories = process_categories(category)
-    return render_template('categories.html', categories=all_categories, name=category)
+    all_sources = process_all_sources()
+    return render_template('categories.html', sources = all_sources, categories=all_categories, name=category)
 
-@app.route('/source/<news_source>')
+@app.route('/sources/<news_source>')
 def news_sources(news_source):
     all_news = process_news_sources(news_source)
-    return render_template('news-source.html', news=all_news, name=news_source)
+    all_sources = process_all_sources()
+    return render_template('news-source.html',  sources = all_sources, news=all_news, name=news_source)
