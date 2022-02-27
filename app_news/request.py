c7f31423b00749b5c165969ff1be41f1cd8ab6f0
Lemmy Mwaura <lemminton@gmail.com>
2022-02-27 11:36:19 +0300

    improved request to fetch news-source and also refactored my views

--- app_news/request.py
+++ app_news/request.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from .models import Articles, Categories
+from .models import Articles, Categories, News_Source
 
 api_key = os.environ.get('NEWS_API_KEY')
 
@@ -26,7 +26,6 @@ def process_articles():
         articles_list.append(news_object)
 
     return articles_list
-# https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=
 
 def get_categories(category):
     url = f'https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={api_key}'
@@ -50,3 +49,26 @@ def process_categories(category):
         category_list.append(category_object)
 
     return category_list
+
+def get_news_sources(news_source):
+    url = f'https://newsapi.org/v2/top-headlines?sources={news_source}&apiKey={api_key}'
+    response = requests.get(url)
+    news_source_dict = response.json()
+
+    return news_source_dict['articles']
+
+def process_news_sources(news_source):
+    news_list = []
+    all_news = get_news_sources(news_source)
+    for article in all_news:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        urltoImage = article.get('urlToImage')
+        publishedAt = article.get('publishedAt')
+        content = article.get('content')
+        news_object = News_Source(author,title,description,url,urltoImage,publishedAt,content)
+        news_list.append(news_object)
+
+    return news_list
--- app_news/views.py
+++ app_news/views.py
@@ -1,9 +1,24 @@
 from flask import render_template
 from app_news import app
-from .request import process_articles
+from .request import process_articles, process_categories, process_news_sources
+from datetime import datetime
 
-@app.route('/home')
 @app.route('/')
+@app.route('/home')
 def index():
     all_articles = process_articles()
-    return render_template('home.html', articles = all_articles)
+    return render_template('home.html', articles = all_articles, datetime1=datetime)
+
+@app.template_filter('datetimeformat')
+def datetimeformat(value, format='%B %d, %Y'):
+    return value.strptime(format)
+
+@app.route('/categories/<category>')
+def categories(category):
+    all_categories = process_categories(category)
+    return render_template('categories.html', categories=all_categories, name=category)
+
+@app.route('/source/<news_source>')
+def news_sources(news_source):
+    all_news = process_news_sources(news_source)
+    return render_template('news-source.html', news=all_news, name=news_source)
